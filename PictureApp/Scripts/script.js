c2be757d56d57c66f4a5e57e5edf9bcd0274ac44
Hervé Claden <hervecladen@gmail.com>
2019-03-28 01:14:39 -0400

    CSS and general HTML formatting

---
+++ PictureApp/Scripts/script.js
@@ -0,0 +1,35 @@
+﻿window.onload = () => {
+    let currentSlide = 0;
+    const slides = document.getElementsByClassName("slide");
+    const buttonPrevious = document.getElementById("previous").addEventListener("click", () => {
+        previousSlide()
+    });
+    const buttonNext = document.getElementById("next").addEventListener("click", () => {
+        nextSlide()
+    });
+
+    switchSlide();
+
+    function previousSlide() {
+        currentSlide--;
+        if (currentSlide < 0) {
+            currentSlide = slides.length - 1;
+        }
+        switchSlide();
+    }
+
+    function nextSlide() {
+        currentSlide++;
+        if (currentSlide > slides.length - 1) {
+            currentSlide = 0;
+        }
+        switchSlide();
+    }
+
+    function switchSlide() {
+        Array.from(slides).forEach(element => {
+            element.style.display = "none"
+        });
+        slides[currentSlide].style.display = "inline-block";
+    }
+}
