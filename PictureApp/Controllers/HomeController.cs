ddd5c4908f8799765048f5c18c7f668a3cbe0af3
Hervé Claden <hervecladen@gmail.com>
2019-03-27 23:27:40 -0400

    Initial Version

--- PictureApp/Controllers/HomeController.cs
+++ PictureApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PictureApp.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,21 +12,12 @@ namespace PictureApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var model = new ImageViewer()
+            {
+                Images = Directory.EnumerateFiles(Server.MapPath("~/Content/Images"))
+                          .Select(fn => "~/Content/Images/" + Path.GetFileName(fn))
+            };
+            return View(model);
         }
     }
 }
